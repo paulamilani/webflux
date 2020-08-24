05a02c0cb0e3696a798b74502027255ab2f729f9
paulamilani <paula.millanii@gmail.com>
2020-08-24 17:25:28 -0300

    projeto api rest webflux

---
+++ README.md
@@ -0,0 +1,10 @@
+#  API REST com Spring Webflux e MongoDB
+
+Projeto spring 
+Mongodb https://cloud.mongodb.com/
+
+IDE spring tool suite
+
+
+https://www.youtube.com/playlist?list=PL8iIphQOyG-CyD9uuRTMiqxEut5QAKHga
+
---
+++ src/main/java/com/apirest/webflux/controller/PlaylistController.java
@@ -0,0 +1,37 @@
+package com.apirest.webflux.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apirest.webflux.document.Playlist;
+import com.apirest.webflux.services.PlaylistService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class PlaylistController {
+
+	@Autowired
+	PlaylistService service;
+
+	@GetMapping(value = "/Playlist")
+	public Flux<Playlist> getPlaylist() {
+		return service.findAll();
+	}
+
+	@GetMapping(value = "/Playlist/{id}")
+	public Mono<Playlist> getPlaylistId(@PathVariable String id) {
+		return service.findById(id);
+	}
+
+	@PostMapping(value = "/Playlist")
+	public Mono<Playlist> save(@RequestBody Playlist playlist) {
+		return service.save(playlist);
+	}
+
+}
